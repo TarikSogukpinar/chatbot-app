5e6f40df88c6e07ed40763fe910f2ae3255cbbbc
TarikSoğukpinar <tariksogukpinar@outlook.com>
2024-09-25 04:47:55 +0300

    init nest js project structure

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: (() => {
+        const env = process.env.NODE_ENV;
+        const envFilePath =
+          env === 'production'
+            ? '.env.production'
+            : env === 'staging'
+              ? '.env.staging'
+              : '.env.development';
+        console.log(`Loading environment variables from ${envFilePath}`);
+        return envFilePath;
+      })(),
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,37 @@
+//Custom Modules, Packages, Configs, etc.
 import { NestFactory } from '@nestjs/core';
+import { Logger, VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+//pnpm packages
+import helmet from 'helmet';
+import * as hpp from 'hpp';
+import * as compression from 'compression';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(6060);
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix(
+    configService.get<string>('API_GLOBAL_PREFIX', { infer: true }),
+  );
+  app.enableShutdownHooks();
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.use(helmet());
+  app.use(hpp());
+  app.use(compression());
+  app.use(cookieParser());
+
+  await app.listen(
+    configService.get<number>('API_PORT', { infer: true }),
+    '0.0.0.0',
+  );
+
+  const apiPort = configService.get<number>('API_PORT', { infer: true });
+  Logger.log(`🚀 Application is running on: http://localhost:${apiPort}/`);
 }
 bootstrap();
